4ee158fde971e128a6cde582c3f5cb6db0f7ddd4
FunsTW <45548858+FunsTW@users.noreply.github.com>
2021-09-24 07:41:36 +0800

    Initial commit

---
+++ Scripts/Projector.cs
@@ -0,0 +1,117 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteAlways]
+public class Projector : MonoBehaviour
+{
+    [Header("Camera Properties")]
+    [SerializeField]private float m_nearClipPlane = 0.1f;
+    public float nearClipPlane {
+        get {
+            if(m_nearClipPlane < 0) {
+                m_nearClipPlane = 0.00001f;
+            }
+            if(m_farClipPlane < m_nearClipPlane) {
+                m_farClipPlane = m_nearClipPlane + 0.1f;
+            }
+            return m_nearClipPlane;
+        }
+        set {
+            m_nearClipPlane = value;
+        }
+    }
+    [SerializeField] private float m_farClipPlane = 50;
+    public float farClipPlane {
+        get {
+            if(m_farClipPlane < 0) {
+                m_farClipPlane = 0.01f;
+            }
+            if(m_farClipPlane < m_nearClipPlane) {
+                m_nearClipPlane = m_farClipPlane - 0.1f;
+            }
+            return m_farClipPlane;
+        }
+        set {
+            m_farClipPlane = value;
+        }
+    }
+    [SerializeField,Range(0.001f,179)] private float m_fieldOfView = 30;
+    public float fieldOfView {
+        get {
+            if(m_fieldOfView < 1) {
+                m_fieldOfView = 1;
+            } else if(m_fieldOfView > 179) {
+                m_fieldOfView = 179;
+            }
+            return m_fieldOfView;
+        }
+        set {
+            m_fieldOfView = value;
+        }
+    }
+    [SerializeField] private float m_aspectRatio = 1;
+    public float aspectRatio {
+        get {
+            return m_aspectRatio;
+        }
+        set {
+            m_aspectRatio = value;
+        }
+    }
+
+    [Header("Projector Pass Setting")]
+    public Material material = null;
+    public Texture projectionImage = null;
+
+    private void Awake() {
+#if UNITY_EDITOR
+        if(!material) {
+            Debug.LogWarning($"{GetType()} : Material not set.", gameObject);
+        }
+#endif
+    }
+
+    private void Update() {
+        if(!material) return;
+
+        //MV
+        var local2World = Matrix4x4.TRS(transform.position, transform.rotation, new Vector3(aspectRatio, 1.0f, 1.0f));
+        print(2 + transform.localToWorldMatrix.ToString("F2"));
+        var world2Local = local2World.inverse;
+        //P
+        var projectorMatrix = Matrix4x4.Perspective(fieldOfView, 1.0f, nearClipPlane, farClipPlane);
+        projectorMatrix *= world2Local;
+
+        //MV
+        var modelClipMatrix = Matrix4x4.identity;
+        modelClipMatrix.SetColumn(3, new Vector4(-nearClipPlane, 0, -nearClipPlane, 1));
+        modelClipMatrix *= world2Local;
+
+        //V
+        var viewClipMatrix = Matrix4x4.identity;
+        viewClipMatrix.SetRow(0,transform.forward);
+        viewClipMatrix.SetRow(1,transform.up);
+        viewClipMatrix.SetRow(2,transform.forward);
+
+        //P
+        var orthoMatrix = Matrix4x4.identity;
+        float size = 1 / (farClipPlane - nearClipPlane);
+        orthoMatrix.m00 = size;
+        orthoMatrix.m11 = size;
+        orthoMatrix.m22 = size;
+
+        var projectorClipMatrix = orthoMatrix * viewClipMatrix* modelClipMatrix;
+
+        material.SetTexture("_MainTex", projectionImage);
+        material.SetMatrix("_ProjectionMatrix", projectorMatrix );
+        material.SetMatrix("_ProjectionClipMatrix",projectorClipMatrix);
+    }
+
+#if UNITY_EDITOR
+    private void OnDrawGizmos() {
+        Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, new Vector3(aspectRatio, 1.0f, 1.0f));
+        Gizmos.DrawFrustum(Vector3.zero, fieldOfView, farClipPlane, nearClipPlane, 1.0f);
+    }
+#endif
+}
